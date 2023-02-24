5b87d621cdeb24f94a9e50c80d66dca783dba1ba
bubblesort <aleksanrmerklin@gmail.com>
2023-02-24 20:43:22 +0300

    Второй пункт 2 лабы

--- lab2/lab2.hs
+++ lab2/lab2.hs
@@ -26,3 +26,46 @@ factorialsList n = scanl (*) 1 [1..n]
 binList :: Int -> [Int]
 binList n = [2^x | x <- [1..n]]
 
+
+-- 2
+
+---- 1
+mean :: [Double] -> Double
+mean list = sum list / fromIntegral (length list)
+
+---- 2
+-- Оператор !! извлекает элемент списка по его индексу
+elemAt :: [Int] -> Int -> Int
+elemAt list n = list !! n
+
+---- 3
+-- 2 : [] -> [2]
+sumLists :: Num a => [a] -> [a] -> [a]
+sumLists [] [] = []
+sumLists [] ys = ys
+sumLists xs [] = xs
+sumLists (x:xs) (y:ys) = (x+y) : sumLists xs ys
+
+---- 6
+removeOdd :: [Int] -> [Int]
+removeOdd list = [x | x <- list, even x]
+
+---- 7
+removeEmpty :: [String] -> [String]
+removeEmpty list = [x | x <- list, length x > 0]
+
+---- 8
+countTrue :: [Bool] -> Int
+countTrue list = length [x | x <- list, x == True]
+
+---- 9
+makePositive :: [Int] -> [Int]
+makePositive list = map abs list
+
+---- 10
+deleteChar :: Char -> [Char] -> [Char]
+deleteChar char word = [x | x <- word, x /= char]
+
+---- 11
+substitute :: Char -> Char -> String -> String
+substitute oldChar newChar string = map (\c -> if c == oldChar then newChar; else c) string
